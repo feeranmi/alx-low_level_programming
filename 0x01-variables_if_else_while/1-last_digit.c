7f356ae706a471ba6639cf856c17f152d9e36970
Feranmi Ajayi <feranmi ajayi>
2023-06-05 09:39:00 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,7 +15,8 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if ((n % 10) > 5
+
+	if ((n % 10) > 5)
 	{
 		printf("last digit of %d is %d and is greater than 5\n", n, n % 10);
 	}
@@ -28,4 +29,5 @@ int main(void)
 		printf("last digit of %d is %d and is 0\n", n, n % 10);
 	}
 	return (0);
+
 }
