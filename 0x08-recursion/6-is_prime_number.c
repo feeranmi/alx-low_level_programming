8cb4efd9c9c5907ea36582b4516e2069ec8e1370
feeranmi <verabiz554@gmail.com>
2023-06-15 09:37:38 -0700

    Recursive Functions

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,29 +1,46 @@
 #include <stdio.h>
 #include "main.h"
 
+int confirm_prime(int n, int a);
+
 /**
   * is_prime_number - Returns 1 if the input integer is a prime number
   * @n: The input interger to check
   *
-  * Return: one for prime number else return zero
+  * Return: 1 for prime number, 0 otherwise
   */
 int is_prime_number(int n)
 {
-	int a;
+	if (n <= 0)
+        {
+                return (0);
+        }
 
-	/** The code base  case */
-	if (n <= 1)
+	return (confirm_prime(n, n - 1));
+}
+
+/**
+  * confirm_prime - Recursive helper function to check if a number is prime
+  * @n: The input integer to check
+  * @a: The divisor to check divisibility
+  *
+  * Return: 1 if n is prime, 0 otherwise
+  */
+int confirm_prime(int n, int a)
+{
+
+	/** Base case */
+	if (a <= 2)
+	{
+		return (1);
+	}
+
+	if (n % a == 0)
 	{
 		return (0);
 	}
 
-	for (a = 2; a <= n - 1; a++)
-	{
-		if (n % a == 0)
-		{
-			return (0);
-		}
-	}
-	return (1);
+	/** Recursive case */
+	return (confirm_prime(n, a - 1));
 
 }
