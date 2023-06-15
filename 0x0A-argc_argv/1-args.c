d374b00d2f280a4a30b867f2663a9cb00710c83a
feeranmi <verabiz554@gmail.com>
2023-06-14 19:01:44 -0700

    trying again

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -10,9 +10,9 @@
  */
 int main(int argc, char *argv[])
 {
-	(void) argv; /* Ignoring argv to focus on argc */
-	printf("The name of the program is: %s\n", argv[0]);
-	printf("The number of command-line arguments is: %i\n", argc - 1);
+	 /* Ignoring argv to focus on argc */
+	(void) argv;
+	printf("%i\n", argc - 1);
 
 	return (0);
 }
