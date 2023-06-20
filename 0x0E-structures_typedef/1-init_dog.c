d255857b6adb1f0b870e3c1e2dc4d3ce16d7085d
feeranmi <verabiz554@gmail.com>
2023-06-20 11:51:25 -0700

    Typedef

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -3,7 +3,7 @@
 
 /**
   * init_dog - Initializes a dog structure with a given value
-  * d: The Pointer to the dog structure to initialize.
+  * @d: The Pointer to the dog structure to initialize.
   * @name: The name of the dog.
   * @age: The age of the dog.
   * @owner: The owner of the dog.
